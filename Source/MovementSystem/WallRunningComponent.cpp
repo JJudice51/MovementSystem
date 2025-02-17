6f02d5604dd6a4eb631845f9c518980de658af48
Justin Judice <jjudice51@gmail.com>
2025-02-16 18:34:01 -0600

    Update WallRunningComponent.cpp
    fix: shifted classes around in cpp for readability
    chore: issues finding a way to bind jump function.
    chore: figure out why the unreal editor keeps deleting my classes in every project I make even though they show up in the solution. Think it has something to do with where its making classes but not sure what setting could be changed to fix that.

--- Source/MovementSystem/WallRunningComponent.cpp
+++ Source/MovementSystem/WallRunningComponent.cpp
@@ -2,10 +2,57 @@
 
 
 #include "WallRunningComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "DrawDebugHelpers.h"
+
+
+// Sets default values for this component's properties
+UWallRunningComponent::UWallRunningComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	//default values for variables
+
+	b_isWallRunning = false;
+	b_isJumping = false;
+	f_wallRunGravityScale = 0.0f;
+	f_normalGravityScale = 1.0f;
+
+}
+
+
+// Called when the game starts
+void UWallRunningComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	//ger owner and movement
+	ac_wallRunCharacter = Cast<ACharacter>(GetOwner());
+	if (ac_wallRunCharacter)
+	{
+		uc_movementComponent = ac_wallRunCharacter->GetCharacterMovement();
+
+		//binding a jump event
+		ac_wallRunCharacter->OnJumped().AddDynamic(this, &UWallRunningComponent::OnJump);
+		ac_wallRunCharacter->LandedDelegate.AddDynamic(this, UWallRunningComponent::OnLanded);
+	}
+
+}
+
+
+// Called every frame
+void UWallRunningComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
 
 void UWallRunningComponent::WallCheck()
 {
-
+	
 }
 
 void UWallRunningComponent::WallRunStart(const FVector& wallNormal)
@@ -24,32 +71,5 @@ void UWallRunningComponent::OnLanded(const FHitResult& Hit)
 {
 }
 
-// Sets default values for this component's properties
-UWallRunningComponent::UWallRunningComponent()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
-}
-
-
-// Called when the game starts
-void UWallRunningComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UWallRunningComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
 
